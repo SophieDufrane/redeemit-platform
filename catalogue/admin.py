bbd98952eeec3060a2dd8cbcb9adec5f19be44d5
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-03 11:25:12 +0000

    Register Reward model in admin, create superuser

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Reward
+
+@admin.register(Reward)
+class RewardAdmin(admin.ModelAdmin):
+
+    list_display = ('reward_name', 'points_cost', 'stock_quantity')
+    search_fields = ('reward_name',)  # A search bar for names
+    list_filter = ('points_cost',)  # Filter by points cost
 
 # Register your models here.
