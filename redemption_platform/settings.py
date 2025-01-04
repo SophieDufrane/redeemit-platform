ec2009cb10441635327346299c2cccb32c11dc4c
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-04 17:44:38 +0000

    Add static and media files

--- redemption_platform/settings.py
+++ redemption_platform/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-sophiedufra-pp4redempti-at0meauvr85.ws.codeinstitute-ide.net', 
@@ -96,6 +96,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+    ]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -130,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- redemption_platform/urls.py
+++ redemption_platform/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogue/', hello_catalogue, name='hello_catalogue'),
 ]
+
