5216a91e4a6a546d2e3f3e87d3ded6abbebf3aba
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-04 18:10:44 +0000

    Set up django-allauth for user authentication

--- redemption_platform/settings.py
+++ redemption_platform/settings.py
@@ -45,11 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-	# 'allauth',
-	# 'allauth.account',
-	# 'allauth.socialaccount',
+	'allauth',
+	'allauth.account',
+	'allauth.socialaccount',
     'catalogue',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -59,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'redemption_platform.urls'
@@ -118,6 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- redemption_platform/urls.py
+++ redemption_platform/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from catalogue.views import hello_catalogue
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('catalogue/', hello_catalogue, name='hello_catalogue'),
+    path("accounts/", include("allauth.urls")),
+    path('admin/', admin.site.urls),
 ]
 
