faade5de4b7656c6c5a21e1805ee6ace68277eb5
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-05 15:02:50 +0000

    Updated model name from Reward to CatalogueItem

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Reward
+from .models import CatalogueItem
 
-@admin.register(Reward)
-class RewardAdmin(admin.ModelAdmin):
+@admin.register(CatalogueItem)
+class CatalogueItemAdmin(admin.ModelAdmin):
 
     list_display = ('reward_name', 'points_cost', 'stock_quantity')
     search_fields = ['reward_name',]
--- catalogue/models.py
+++ catalogue/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Reward(models.Model):
+class CatalogueItem(models.Model):
     reward_name = models.CharField(max_length=100, unique=True)
     points_cost = models.PositiveIntegerField(default=0)
     description = models.TextField()
