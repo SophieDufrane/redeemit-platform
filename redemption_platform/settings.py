fbe38a1bde37ea293d93bec9c752d595c3ae5dbb
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-09 19:27:56 +0000

    Fix style footer and catalogue section. Add specific gift cards pictures from admin dashboard

--- redemption_platform/settings.py
+++ redemption_platform/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-sophiedufra-pp4redempti-at0meauvr85.ws.codeinstitute-ide.net', 
@@ -146,6 +146,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
--- redemption_platform/urls.py
+++ redemption_platform/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from catalogue.views import catalogue_home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('catalogue/', catalogue_home, name='catalogue_home'),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
