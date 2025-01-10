fb9c3fd34ebf611983281a4809dbfb8a6ebcd076
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-10 12:54:35 +0000

    Add catalogue_detail view and refine catalogue_home for clarity

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import CatalogueItem
 
 def catalogue_home(request):
-    items = CatalogueItem.objects.all()
-    return render(request, 'catalogue/catalogue.html', {'items': items})
+    catalogue_items = CatalogueItem.objects.all()
+    return render(request, 'catalogue/catalogue.html', {'catalogue_items': catalogue_items})
+
+def catalogue_detail(request, slug):
+    selected_item = get_object_or_404(CatalogueItem, slug=slug)  # Fetch item by slug
+    return render(request, 'catalogue/catalogue_detail.html', {'selected_item': selected_item})
