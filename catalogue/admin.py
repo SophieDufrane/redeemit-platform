dd884481d5a933ddd0b5ab57ed4147f838184614
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-10 12:22:26 +0000

    Add prepopulated slug field to CatalogueItemAdmin

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -4,7 +4,8 @@ from .models import CatalogueItem
 @admin.register(CatalogueItem)
 class CatalogueItemAdmin(admin.ModelAdmin):
 
-    list_display = ('reward_name', 'points_cost', 'stock_quantity', 'reward_Terms_And_Conditions')
+    list_display = ('reward_name', 'slug', 'points_cost', 'stock_quantity', 'reward_Terms_And_Conditions')
+    prepopulated_fields = {'slug': ('reward_name',)}
     search_fields = ['reward_name',]
     list_filter = ('points_cost',)
 
