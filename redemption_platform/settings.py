6c806433370d898233656ff151d6c0b39981dfd3
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-13 10:31:12 +0000

    Update settings.py to use DEBUG environment variable

--- redemption_platform/settings.py
+++ redemption_platform/settings.py
@@ -155,3 +155,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DEBUG Settings
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+
+
