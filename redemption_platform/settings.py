31b23483f523c0f0e02961f0fd70a3e1ec8def45
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-16 08:58:01 +0000

    Add home page and set up homepage view, template and URL

--- redemption_platform/settings.py
+++ redemption_platform/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 	'allauth.socialaccount',
     'cloudinary',
     'catalogue',
+    'home',
 ]
 
 SITE_ID = 1
@@ -153,5 +154,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+DEBUG = os.environ.get("DEBUG") == "True"
 
