3e60e64d1f5a1a0da4ef2b44d1d890afa8285ab0
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-19 16:52:08 +0000

    Fix admin configuration. Add 'get' methods to retrieve default User parameters

--- home/admin.py
+++ home/admin.py
@@ -4,26 +4,35 @@ from .models import UserProfile
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = (
-        'user',
-        'first_name',
-        'last_name',
-        'email',
-        'point_balance',
+        'user', 
+        'get_first_name',
+        'get_last_name', 
+        'get_email', 
+        'point_balance', 
         'join_date'
     )
     fields = (
-        'user',
-        'first_name', 
-        'last_name', 
-        'email',
-        'point_balance',
+        'user', 
+        'point_balance', 
         'join_date',
     )
     readonly_fields = ('join_date',)
     search_fields = (
-        'user__username',
-        'email',
-        'first_name',
-        'last_name'
+        'user__username', 
+        'user__email', 
+        'user__first_name', 
+        'user__last_name'
     )
     list_filter = ('join_date',)
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+    get_first_name.short_description = 'First Name'
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
+    get_last_name.short_description = 'Last Name'
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
