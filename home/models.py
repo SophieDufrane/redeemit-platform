d42dbee0253a6a6c42c47a54829a019ba82c720a
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-19 15:37:31 +0000

    Create Userprofile model extending Django default user model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    """
+    Extends the default User model with additional fields.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    point_balance = models.IntegerField(default=0)
+    join_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+         return self.user.username
