8cdc52e218de22c859dff82524e33d86dcc8a476
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-19 16:20:05 +0000

    Add UserProfile admin configuration for user and points balance management

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -3,8 +3,22 @@ from .models import CatalogueItem
 
 @admin.register(CatalogueItem)
 class CatalogueItemAdmin(admin.ModelAdmin):
-
-    list_display = ('reward_name', 'points_cost', 'stock_quantity', 'reward_value')
-    fields = ('reward_name', 'slug', 'points_cost', 'description', 'image', 'image_description', 'stock_quantity', 'reward_value', 'reward_terms_and_conditions')
+    list_display = (
+        'reward_name',
+        'points_cost',
+        'stock_quantity',
+        'reward_value'
+        )
+    fields = (
+        'reward_name',
+        'slug',
+        'points_cost',
+        'description',
+        'image',
+        'image_description',
+        'stock_quantity',
+        'reward_value',
+        'reward_terms_and_conditions'
+        )
     prepopulated_fields = {'slug': ('reward_name',)}
-    search_fields = ['reward_name',]
+    search_fields = ('reward_name',)
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'first_name',
+        'last_name',
+        'email',
+        'point_balance',
+        'join_date'
+    )
+    fields = (
+        'user',
+        'first_name', 
+        'last_name', 
+        'email',
+        'point_balance',
+        'join_date',
+    )
+    readonly_fields = ('join_date',)
+    search_fields = (
+        'user__username',
+        'email',
+        'first_name',
+        'last_name'
+    )
+    list_filter = ('join_date',)
