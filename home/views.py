77bcb40590f67a642a400970cbd803f82937f200
Sophie Dufrane <dufranesophie@gmail.com>
2025-01-30 17:02:38 +0000

    Add signup_view for user registration with CustomSignupForm

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomSignupForm
 
 def home(request):
     """
@@ -9,3 +11,29 @@ def home(request):
 
     """
     return render(request, 'home.html')
+
+
+def signup_view(request):
+    """
+    Handles user's registration with custom form.
+
+    - Displays the signup form when accessed via GET.
+    - Processes form submission when accessed via POST.
+    - Creates a new user, hashes their password, and logs them in.
+    """
+    if request == "POST":
+        # Populate form with user input
+        form = CustomSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            # Don't save yet (we need to hash the password)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            login(request, user)
+            return redirect("home")
+    
+    else:
+        # If GET request, show an empty form
+        form = CustomSignupForm()
+    
+    return render(request, "account/signup.html", {"form": form})
