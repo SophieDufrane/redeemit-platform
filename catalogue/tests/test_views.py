df27e3377ac8fbe3627d28172ffa23d5c035bd2c
Sophie Dufrane <dufranesophie@gmail.com>
2025-02-14 18:05:18 +0000

    Test: Add views tests for cart displays added rewards

--- catalogue/tests/test_views.py
+++ catalogue/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from catalogue.models import CatalogueItem
+from catalogue.models import CatalogueItem, Cart, CartItem
 
 
 class CatalogueViewsTests(TestCase):
@@ -41,4 +41,46 @@ class CatalogueViewsTests(TestCase):
             "Test Reward",
             msg_prefix="Catalogue home does not display "
             "available rewards when it should."
+        )
+
+class CartViewsTests(TestCase):
+    def setUp(self):
+        """Create a test user and a sample reward"""
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        self.item = CatalogueItem.objects.create(
+            reward_name="Test Reward",
+            slug="test-reward",
+            points_cost=100,
+            stock_quantity=5
+        )
+
+        self.cart = Cart.objects.create(user=self.user)
+        self.cart_item = CartItem.objects.create(
+            cart=self.cart,
+            item=self.item,
+            quantity=2
+        )
+    
+    def test_cart_displays_items(self):
+        """Test if cart page displays added rewards"""
+        self.client.login(username='testuser', password='testpassword')
+        self.cart.refresh_from_db()
+        self.cart_item.refresh_from_db()
+        response = self.client.get(reverse('cart_page'))
+
+        self.assertContains(
+            response,
+            "Test Reward",
+            msg_prefix="Cart page does not display "
+            "added rewards when it should."
+        )
+
+        self.assertContains(
+            response,
+            "2",
+            msg_prefix="Cart page does not display correct quantity."
         )
